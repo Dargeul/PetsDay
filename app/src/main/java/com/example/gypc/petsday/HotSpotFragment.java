1e2868c532a413f6113d42df615005f588df9e56
郭瑶佳 <1483197714@qq.com>
2017-12-07 22:48:06 +0800

    实现页面跳转，添加了三个fragment（动态，商城和我的），有对应的布局和代码文件。

---
+++ app/src/main/java/com/example/gypc/petsday/HotSpotFragment.java
@@ -0,0 +1,20 @@
+package com.example.gypc.petsday;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by gypc on 2017/12/7.
+ */
+
+public class HotSpotFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_hotspot, container, false);
+    }
+}
--- app/src/main/java/com/example/gypc/petsday/MainActivity.java
+++ app/src/main/java/com/example/gypc/petsday/MainActivity.java
@@ -1,31 +1,32 @@
 package com.example.gypc.petsday;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private ViewPager viewPager;
+    private MenuItem menuItem;
+    private BottomNavigationView navigation;
 
+    //为底部导航栏添加监听，当导航栏有所改动时，设置相应的Fragment
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_hotspot:
-                    mTextMessage.setText(R.string.title_home);
+                    viewPager.setCurrentItem(0);
                     return true;
                 case R.id.navigation_market:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    viewPager.setCurrentItem(1);
                     return true;
                 case R.id.navigation_mine:
-                    mTextMessage.setText(R.string.title_notifications);
+                    viewPager.setCurrentItem(2);
                     return true;
             }
             return false;
@@ -33,14 +34,44 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
+    //为viewPager添加监听，设置当viewpager有所变动时，更改导航栏的选项
+    private ViewPager.OnPageChangeListener mOnPageChangeListener
+            = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        }
+        @Override
+        public void onPageSelected(int position) {
+            if (menuItem != null) {
+                menuItem.setChecked(false);
+            } else {
+                navigation.getMenu().getItem(0).setChecked(false);
+            }
+            menuItem = navigation.getMenu().getItem(position);
+            menuItem.setChecked(true);
+        }
+        @Override
+        public void onPageScrollStateChanged(int state) {
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        viewPager = (ViewPager) findViewById(R.id.vPager);
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        viewPager.addOnPageChangeListener(mOnPageChangeListener);
+        setupViewPager(viewPager);
     }
-
+    //添加fragment，进行页面切换
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HotSpotFragment());
+        adapter.addFragment(new MarketFragment());
+        adapter.addFragment(new MineFragment());
+        viewPager.setAdapter(adapter);
+    }
 }
