9a2734922f28c91617da6ba82b5250f895831bd7
郭瑶佳 <1483197714@qq.com>
2017-12-11 22:15:37 +0800

    添加跳转到发布动态的逻辑。

--- app/src/main/java/com/example/gypc/petsday/HotSpotFragment.java
+++ app/src/main/java/com/example/gypc/petsday/HotSpotFragment.java
@@ -1,20 +1,37 @@
 package com.example.gypc.petsday;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Created by gypc on 2017/12/7.
  */
 
 public class HotSpotFragment extends Fragment {
+    private ImageView addIV;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_hotspot, container, false);
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        addIV =(ImageView) getView().findViewById(R.id.publish);
+        addIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),PublishActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/gypc/petsday/PublishActivity.java
@@ -0,0 +1,16 @@
+package com.example.gypc.petsday;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gypc on 2017/12/11.
+ */
+
+public class PublishActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hotspot_publish);
+    }
+}
