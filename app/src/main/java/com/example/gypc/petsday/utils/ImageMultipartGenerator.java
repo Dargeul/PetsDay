5cb521001067999c30f4d9dcbf02318db2a56ac1
xujj25 <xujj25@foxmail.com>
2017-12-25 17:36:33 +0800

    添加图像表单处理组件

---
+++ app/src/main/java/com/example/gypc/petsday/utils/ImageMultipartGenerator.java
@@ -0,0 +1,37 @@
+package com.example.gypc.petsday.utils;
+
+import android.util.Log;
+
+import java.io.File;
+import java.util.List;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.http.Multipart;
+
+/**
+ * Created by XUJIJUN on 2017/12/25.
+ */
+
+public class ImageMultipartGenerator {
+
+    private static final String IMG_NAME = "fulAvatar"; //后台接收图片流的参数名
+
+    public static List<MultipartBody.Part> getParts(String filePath) {
+        try {
+            File file = new File(filePath);//filePath 图片地址
+            MultipartBody.Builder builder = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM);//表单类型
+            RequestBody imageBody = RequestBody.create(MediaType.parse("image/jpeg"), file);
+            builder.addFormDataPart(IMG_NAME, file.getName(), imageBody);
+
+            List<MultipartBody.Part> parts = builder.build().parts();
+
+            return parts;
+        } catch (Exception e) {
+            Log.e("ImageMultipartGenerator", "getParts", e);
+            return null;
+        }
+    }
+}
