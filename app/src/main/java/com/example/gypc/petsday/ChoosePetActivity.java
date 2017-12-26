14b3a1f63b291d9072ffa7d907db3e03e1542176
郭瑶佳 <1483197714@qq.com>
2017-12-27 00:15:43 +0800

    修改了发布动态到选择宠物的跳转逻辑。

--- app/src/main/java/com/example/gypc/petsday/ChoosePetActivity.java
+++ app/src/main/java/com/example/gypc/petsday/ChoosePetActivity.java
@@ -2,6 +2,22 @@ package com.example.gypc.petsday;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.example.gypc.petsday.adapter.ChoosePetAdapter;
+import com.example.gypc.petsday.model.Pet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by gypc on 2017/12/13.
@@ -9,9 +25,73 @@ import android.support.v7.app.AppCompatActivity;
 
 public class ChoosePetActivity extends AppCompatActivity {
 
+    private ImageView finishChooseIV;
+    private ImageView backIV;
+    private RecyclerView petList;
+    private ChoosePetAdapter choosePetAdapter;
+
+    private List<Pet> pets;
+    private List<Pet> pets_choose;
+
+
+    public void initWidget(){
+        finishChooseIV = (ImageView)findViewById(R.id.finishChoose);
+        backIV = (ImageView)findViewById(R.id.back);
+        petList = (RecyclerView) findViewById(R.id.petList);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hotspot_publish_choosepet);
+
+        initWidget();
+
+        //----------测试用------------
+        pets = new ArrayList<Pet>(){
+            {
+                add(new Pet("Jackieee","哈士奇",320));
+                add(new Pet("Kackieee","中华田园犬",320));
+            }
+        };
+        pets_choose = new ArrayList<Pet>();
+        //----------测试用-----------
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(ChoosePetActivity.this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        choosePetAdapter = new ChoosePetAdapter(R.layout.hotspot_publish_pet);
+
+        choosePetAdapter.addData(pets);
+        petList.setLayoutManager(layoutManager);
+        petList.setAdapter(choosePetAdapter);
+
+        choosePetAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                //checkbox状态更改
+                CheckBox c = (CheckBox) view.findViewById(R.id.checkbox);
+                c.setChecked(!c.isChecked());
+
+                //--------测试数据添加---------
+                if(c.isChecked()){
+                    pets_choose.add(pets.get(position));
+                    Toast.makeText(ChoosePetActivity.this,pets.get(position).getPet_nickname()+"被选中",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    pets_choose.remove(pets.get(position));
+                    Toast.makeText(ChoosePetActivity.this,pets.get(position).getPet_nickname()+"去除选中",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        //确定键，点击完数据传回发布动态页面
+        finishChooseIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/gypc/petsday/PublishActivity.java
+++ app/src/main/java/com/example/gypc/petsday/PublishActivity.java
@@ -78,7 +78,7 @@ public class PublishActivity extends BaseActivity {
         choosePet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(PublishActivity.this,NotificationActivity.class);
+                Intent intent = new Intent(PublishActivity.this,ChoosePetActivity.class);
                 startActivity(intent);
             }
         });
