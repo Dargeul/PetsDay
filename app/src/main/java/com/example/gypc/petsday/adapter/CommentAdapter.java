63059af6db5c500d27e8a5731eaecab43e5ccc8b
郭瑶佳 <1483197714@qq.com>
2017-12-28 11:33:01 +0800

    添加了comment的model和adapter 动态详情中，点击评论，可进行回复；点击评论图片或者最底部edittext，可以输入评论。

---
+++ app/src/main/java/com/example/gypc/petsday/adapter/CommentAdapter.java
@@ -0,0 +1,34 @@
+package com.example.gypc.petsday.adapter;
+
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.example.gypc.petsday.R;
+import com.example.gypc.petsday.model.Comment;
+
+import java.util.List;
+
+/**
+ * Created by gypc on 2017/12/28.
+ */
+
+public class CommentAdapter extends BaseQuickAdapter<Comment,BaseViewHolder> {
+    public CommentAdapter(@LayoutRes int layoutResId, @Nullable List<Comment> data){
+        super(layoutResId, data);
+        }
+
+    public CommentAdapter(@LayoutRes int layoutResId){
+        super(layoutResId);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper,Comment item){
+
+        helper.setText(R.id.userNickname, item.getCom_user());
+        helper.setText(R.id.commentTime,item.getCom_time());
+        helper.setText(R.id.comment,item.getCom_content());
+        int position = helper.getLayoutPosition();
+    }
+}
