2c910509f17b7bb85b039d976a1a3ca77bdd42a5
xujj25 <xujj25@foxmail.com>
2018-01-03 18:33:41 +0800

    根据Hotspot修改内容，修改接口

--- app/src/main/java/com/example/gypc/petsday/adapter/HotSpotAdapter.java
+++ app/src/main/java/com/example/gypc/petsday/adapter/HotSpotAdapter.java
@@ -11,7 +11,7 @@ import com.bumptech.glide.Priority;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.example.gypc.petsday.R;
-import com.example.gypc.petsday.model.hotspot;
+import com.example.gypc.petsday.model.Hotspot;
 import com.example.gypc.petsday.utils.ImageUriConverter;
 
 import java.util.List;
@@ -20,8 +20,8 @@ import java.util.List;
  * Created by gypc on 2017/12/21.
  */
 
-public class HotSpotAdapter extends BaseQuickAdapter<hotspot,BaseViewHolder> {
-    public HotSpotAdapter(@LayoutRes int layoutResId, @Nullable List<hotspot>data){
+public class HotSpotAdapter extends BaseQuickAdapter<Hotspot,BaseViewHolder> {
+    public HotSpotAdapter(@LayoutRes int layoutResId, @Nullable List<Hotspot>data){
         super(layoutResId, data);
     }
 
@@ -30,7 +30,7 @@ public class HotSpotAdapter extends BaseQuickAdapter<hotspot,BaseViewHolder> {
     * */
 
     @Override
-    protected void convert(BaseViewHolder helper,hotspot item){
+    protected void convert(BaseViewHolder helper,Hotspot item){
         //将xml中的数据项和model里面的数据绑在一起
         helper.setText(R.id.hotSpotContent, item.getHs_content());
 //        Glide.with(this.mContext).load(item.getHs_photo()).priority(Priority.HIGH)
@@ -54,8 +54,8 @@ public class HotSpotAdapter extends BaseQuickAdapter<hotspot,BaseViewHolder> {
 
         helper.setText(R.id.userNickname, "" + item.getHs_user());
         helper.setText(R.id.publishTime, item.getHs_time());
-        helper.setText(R.id.likeNum, "" + item.getHs_like());
-        helper.setText(R.id.commentNum, "" + item.getHs_comment());
+//        helper.setText(R.id.likeNum, "" + item.getHs_like());
+//        helper.setText(R.id.commentNum, "" + item.getHs_comment());
         int position = helper.getLayoutPosition();
     }
 }
