f7545144b34ccd520b83766c2f6ea15d275487fc
郭瑶佳 <1483197714@qq.com>
2018-01-03 21:34:35 +0800

    修改评论model里的com_user为int

--- app/src/main/java/com/example/gypc/petsday/model/Comment.java
+++ app/src/main/java/com/example/gypc/petsday/model/Comment.java
@@ -6,12 +6,12 @@ package com.example.gypc.petsday.model;
 
 public class Comment {
     private String com_time;
-    private String com_user;
+    private int com_user;
     private int com_hs;  //指向动态表的id
     private String com_content;
     private int com_id;
 
-    public Comment(int com_id,int com_hs,String com_user,String com_time,String com_content){
+    public Comment(int com_id,int com_hs,int com_user,String com_time,String com_content){
         this.com_id = com_id;
         this.com_hs = com_hs;
         this.com_user = com_user;
@@ -19,7 +19,7 @@ public class Comment {
         this.com_content = com_content;
     }
 
-    public String getCom_user(){
+    public int getCom_user(){
         return com_user;
     }
 
