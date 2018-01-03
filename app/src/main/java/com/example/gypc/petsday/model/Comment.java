3bbda59661efa611b6a2552becb45465bbc17133
xujj25 <xujj25@foxmail.com>
2018-01-03 18:32:13 +0800

    修改关联动态id字段名字

--- app/src/main/java/com/example/gypc/petsday/model/Comment.java
+++ app/src/main/java/com/example/gypc/petsday/model/Comment.java
@@ -5,15 +5,15 @@ package com.example.gypc.petsday.model;
  */
 
 public class Comment {
-    private String com_user;
-    private int com_walk;  //指向动态表的id
-    private int com_id;
     private String com_time;
+    private String com_user;
+    private int com_hs;  //指向动态表的id
     private String com_content;
+    private int com_id;
 
-    public Comment(int com_id,int com_walk,String com_user,String com_time,String com_content){
+    public Comment(int com_id,int com_hs,String com_user,String com_time,String com_content){
         this.com_id = com_id;
-        this.com_walk = com_walk;
+        this.com_hs = com_hs;
         this.com_user = com_user;
         this.com_time = com_time;
         this.com_content = com_content;
@@ -23,8 +23,8 @@ public class Comment {
         return com_user;
     }
 
-    public int getCom_walk(){
-        return com_walk;
+    public int getCom_hs(){
+        return com_hs;
     }
 
     public int getCom_id(){
