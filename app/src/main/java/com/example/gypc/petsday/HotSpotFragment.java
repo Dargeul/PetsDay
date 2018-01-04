d3147a440d8fa4166159b5dc144ee48e51327e15
xujj25 <xujj25@foxmail.com>
2018-01-04 16:56:02 +0800

    添加从远端加载动态列表逻辑

--- app/src/main/java/com/example/gypc/petsday/HotSpotFragment.java
+++ app/src/main/java/com/example/gypc/petsday/HotSpotFragment.java
@@ -27,12 +27,18 @@ import java.util.Date;
 import java.util.List;
 
 import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by gypc on 2017/12/7.
  */
 
 public class HotSpotFragment extends Fragment {
+
+    private static final int HOTSPOT_DETAIL_REQ_CODE = 1;
+
     private ImageView addIV;
     private ImageView notificationIV;
     private RecyclerView hotSpotRecyclerView;
@@ -45,7 +51,9 @@ public class HotSpotFragment extends Fragment {
 
     private static HotSpotFragment instance;
 
-    private ObjectService objectService;
+    private ObjectService objectService = ObjectServiceFactory.getService();
+
+    private int pageNumber = 0;
 
     @Nullable
     @Override
@@ -65,7 +73,6 @@ public class HotSpotFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         app = AppContext.getInstance();
-        objectService = ObjectServiceFactory.getService();
 
         initWidget();
 
@@ -90,12 +97,9 @@ public class HotSpotFragment extends Fragment {
         * 动态列表的数据类型是hotspot这个model
         * */
         //final Bitmap bmp = BitmapFactory.decodeResource(getResources(),R.drawable.pet_photo_1);
-        final String bmp = "keji";
-        datas = app.getDatas();
-        datas.add(new Hotspot(new Date().toString(), 1, "I like it", 1, bmp, 888));
-        datas.add(new Hotspot(new Date().toString(), 1, "I like it", 1, bmp, 888));
-        datas.add(new Hotspot(new Date().toString(), 1, "I like it", 1, bmp, 888));
-        datas.add(new Hotspot(new Date().toString(), 1, "I like it", 1, bmp, 888));
+//        final String bmp = "keji";
+        datas = app.getInitHotspots();
+        Log.i("HotSpotFragment", "onActivityCreated: datas.size() = " + datas.size());
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
@@ -112,7 +116,13 @@ public class HotSpotFragment extends Fragment {
             @Override
             public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
                 Intent intent = new Intent(getActivity(),HotSpotDetailActivity.class);
-                startActivity(intent);
+
+                Hotspot item = (Hotspot) adapter.getItem(position);
+                Bundle bundle = item.getBundle();
+
+                intent.putExtras(bundle);
+
+                getActivity().startActivityForResult(intent, HOTSPOT_DETAIL_REQ_CODE);
                 Toast.makeText(getActivity(), "HotSpot:" + position, Toast.LENGTH_SHORT).show();
             }
         });
@@ -124,26 +134,10 @@ public class HotSpotFragment extends Fragment {
         easyRefreshLayout.addEasyEvent(new EasyRefreshLayout.EasyEvent() {
             @Override
             public void onLoadMore() {
-                final List<Hotspot> list = new ArrayList<>();
-                //final Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.pet_photo_2);
-                final String bitmap = "keji";
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        //添加2个列表项到动态的数据列表中
-                        for (int j = 0; j < 2; j++) {
-                            list.add(new Hotspot(new Date().toString(), 1, "Loaded", 1, bitmap, 888));
-                            datas.add(new Hotspot(new Date().toString(), 1, "Loaded", 1, bitmap, 888));
-                        }
-                        easyRefreshLayout.loadMoreComplete(new EasyRefreshLayout.Event() {
-                            @Override
-                            public void complete() {
-                                hotSpotAdapter.getData().addAll(list);
-                                hotSpotAdapter.notifyDataSetChanged();
-                                Toast.makeText(getContext(), "load success", Toast.LENGTH_SHORT).show();
-                            }
-                        }, 500);
-
+                        getRemoteOldHotspot();
                     }
                 }, 2000);
 
@@ -157,20 +151,7 @@ public class HotSpotFragment extends Fragment {
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        //添加2个列表项到动态的数据列表中
-                        //final Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.pet_photo_3);
-                        final String bitmap = "keji";
-                        List<Hotspot> list = new ArrayList<>();
-                        for (int i = 0; i < 2; i++) {
-                            list.add(new Hotspot(new Date().toString(), 1, "Refresh", 1, bitmap, 888));
-                        }
-                        list.addAll(datas);
-                        datas.removeAll(datas);
-                        datas.addAll(list);
-                        //刷新每次都用setNewData重新加载数据
-                        hotSpotAdapter.setNewData(list);
-                        easyRefreshLayout.refreshComplete();
-                        Toast.makeText(getContext(), "refresh success", Toast.LENGTH_SHORT).show();
+                        getRemoteNewHotspot();
                     }
                 }, 1000);
             }
@@ -178,6 +159,77 @@ public class HotSpotFragment extends Fragment {
 
         hotSpotRecyclerView.setAdapter(hotSpotAdapter);
 
+    }
+
+    private void getRemoteOldHotspot() {
+        ++pageNumber;
+        objectService
+                .getHotspotListByPageNumber(String.valueOf(pageNumber))
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<List<Hotspot>>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.i("HotSpotFragment", "getRemoteOldHotspot: complete");
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("HotSpotFragment", "getRemoteOldHotspot", throwable);
+                    }
+
+                    @Override
+                    public void onNext(List<Hotspot> hotspots) {
+                        Log.i("HotSpotFragment", "getRemoteOldHotspot: onNext: list size = " + String.valueOf(hotspots.size()));
+                        //添加2个列表项到动态的数据列表中
+//                        for (int j = 0; j < 2; j++) {
+//                            list.add(new Hotspot(new Date().toString(), 1, "Loaded", 1, bitmap, 888));
+//                            datas.add(new Hotspot(new Date().toString(), 1, "Loaded", 1, bitmap, 888));
+//                        }
+                        final List<Hotspot> list = new ArrayList<Hotspot>();
+                        list.addAll(hotspots);
+                        datas.addAll(hotspots);
+                        easyRefreshLayout.loadMoreComplete(new EasyRefreshLayout.Event() {
+                            @Override
+                            public void complete() {
+                                hotSpotAdapter.getData().addAll(list);
+                                hotSpotAdapter.notifyDataSetChanged();
+                                Toast.makeText(getContext(), "load success", Toast.LENGTH_SHORT).show();
+                            }
+                        }, 500);
+                    }
+                });
+    }
+
+    private void getRemoteNewHotspot() {
+        objectService
+                .getNewestHotspotList()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<List<Hotspot>>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.i("HotSpotFragment", "getRemoteNewHotspot: complete");
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("HotSpotFragment", "getRemoteNewHotspot", throwable);
+                    }
+
+                    @Override
+                    public void onNext(List<Hotspot> hotspots) {
+                        Log.i("HotSpotFragment", "getRemoteNewHotspot: onNext: list size = " + String.valueOf(hotspots.size()));
+                        datas.removeAll(hotspots);
+                        hotspots.addAll(datas);
+                        datas.removeAll(datas);
+                        datas.addAll(hotspots);
+                        //刷新每次都用setNewData重新加载数据
+                        hotSpotAdapter.setNewData(hotspots);
+                        easyRefreshLayout.refreshComplete();
+                        Toast.makeText(getContext(), "refresh success", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     public static HotSpotFragment getInstance() {
@@ -197,5 +249,10 @@ public class HotSpotFragment extends Fragment {
                 "photo = " + bundle.getString("photo") + "\n" +
                 "time = " + bundle.getString("time");
         Log.i("HotSpotFragment", "addHotspot: data:\n" + data);
+    }
+
+    public void initDatas(List<Hotspot> datas) {
+        this.datas = datas;
+        hotSpotAdapter.setNewData(datas);
     }
 }
