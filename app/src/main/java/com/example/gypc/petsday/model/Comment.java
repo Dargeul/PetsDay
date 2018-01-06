2bbc3aa7da80484e9201ed704496902cc75ccd1f
xujj25 <xujj25@foxmail.com>
2018-01-06 20:03:47 +0800

    添加用户昵称字段

--- app/src/main/java/com/example/gypc/petsday/model/Comment.java
+++ app/src/main/java/com/example/gypc/petsday/model/Comment.java
@@ -10,13 +10,15 @@ public class Comment {
     private int com_hs;  //指向动态表的id
     private String com_content;
     private int com_id;
+    private String user_nickname;
 
-    public Comment(int com_id,int com_hs,int com_user,String com_time,String com_content){
+    public Comment(int com_id,int com_hs,int com_user,String com_time,String com_content, String user_nickname){
         this.com_id = com_id;
         this.com_hs = com_hs;
         this.com_user = com_user;
         this.com_time = com_time;
         this.com_content = com_content;
+        this.user_nickname = user_nickname;
     }
 
     public int getCom_user(){
@@ -37,5 +39,9 @@ public class Comment {
 
     public String getCom_content(){
         return com_content;
+    }
+
+    public String getUser_nickname() {
+        return user_nickname;
     }
 }
