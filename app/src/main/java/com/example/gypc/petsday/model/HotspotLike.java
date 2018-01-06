e3a9997d50fa4970a01b239886060feae6cf109a
xujj25 <xujj25@foxmail.com>
2018-01-06 14:54:21 +0800

    构造函数设为public

--- app/src/main/java/com/example/gypc/petsday/model/HotspotLike.java
+++ app/src/main/java/com/example/gypc/petsday/model/HotspotLike.java
@@ -9,7 +9,7 @@ public class HotspotLike {
     private int like_hotspot;
     private int like_id;
 
-    HotspotLike(int like_id, int like_hotspot, int like_user) {
+    public HotspotLike(int like_id, int like_hotspot, int like_user) {
         this.like_id = like_id;
         this.like_hotspot = like_hotspot;
         this.like_user = like_user;
