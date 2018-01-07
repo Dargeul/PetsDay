d4f8f340031a6b94dbc61f08a2eda0a121dff931
郭瑶佳 <1483197714@qq.com>
2018-01-07 14:05:38 +0800

    添加了设置为已读的网络请求。

--- app/src/main/java/com/example/gypc/petsday/NotificationActivity.java
+++ app/src/main/java/com/example/gypc/petsday/NotificationActivity.java
@@ -9,17 +9,23 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.example.gypc.petsday.adapter.NotificationAdapter;
 import com.example.gypc.petsday.factory.ObjectServiceFactory;
+import com.example.gypc.petsday.model.Hotspot;
+import com.example.gypc.petsday.model.RemoteDBOperationResponse;
 import com.example.gypc.petsday.model.UserNotification;
 import com.example.gypc.petsday.service.ObjectService;
 import com.example.gypc.petsday.utils.AppContext;
+import com.example.gypc.petsday.utils.JSONRequestBodyGenerator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import rx.Subscriber;
@@ -34,6 +40,7 @@ public class NotificationActivity extends AppCompatActivity {
     private RecyclerView notificationsRV;
     private RecyclerView notificationsReadedRV;
     private TextView tips;
+    private ImageView backIV;
 
     private NotificationAdapter notificationAdapter;
     private NotificationAdapter notificationReadedAdapter;
@@ -55,8 +62,10 @@ public class NotificationActivity extends AppCompatActivity {
 
         init();
 
-        //网络请求
-        getNotification();
+        //区分已读和未读通知
+        identifyNotification();
+        //设置两种通知列表的adapter
+        setNoticeAdapter();
 
     }
 
@@ -72,44 +81,52 @@ public class NotificationActivity extends AppCompatActivity {
 
         notificationsRV = (RecyclerView)findViewById(R.id.notificationRV);
         notificationsReadedRV = (RecyclerView)findViewById(R.id.notificationReadedRV);
-
+        backIV = (ImageView)findViewById(R.id.back);
         tips = (TextView)findViewById(R.id.tips);
+
+        backIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeNotificationState();
+                finish();
+            }
+        });
     }
 
-    private void getNotification(){
-        userId = (int) app.getLoginUserInfo().get("user_id");
+
+    public void getHotSpot(String hotspotId){
         objectService
-                .getNotificationByUserId(userId+"")
+                .getHotspotById(hotspotId)
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<List<UserNotification>>() {
+                .subscribe(new Subscriber<List<Hotspot>>() {
                     @Override
                     public void onCompleted() {
-
                     }
 
                     @Override
                     public void onError(Throwable e) {
-                        Log.e("NotificationActivity","getNotification",e);
+                        Log.e("NotificationActivity","getHotSpot",e);
                     }
 
                     @Override
-                    public void onNext(List<UserNotification> userNotifications) {
+                    public void onNext(List<Hotspot> hotspots) {
                         try{
-                            notifications.removeAll(notifications);
-                            notifications.addAll(userNotifications);
-                            //区分已读和未读通知
-                            identifyNotification();
-                            //设置两种通知列表的adapter
-                            setNoticeAdapter();
+                            Intent intent = new Intent(NotificationActivity.this,HotSpotDetailActivity.class);
+
+                            Bundle bundle = hotspots.get(0).getBundle();
+                            bundle.putBoolean("fromNotificationActivity", true);
+                            intent.putExtras(bundle);
+
+                            startActivityForResult(intent,MainActivity.HOTSPOT_DETAIL_REQ_CODE);
                         }catch (Exception e){
-                            Log.e("NotificationActivity","getNotification",e);
+                            Log.e("NotificationActivity","getHotSpot",e);
                         }
                     }
                 });
     }
 
-    private void identifyNotification(){
+    public void identifyNotification(){
         if(notifications.size() == 0){
             tips.setText("---当前没有任何通知---");
         }else{
@@ -138,8 +155,7 @@ public class NotificationActivity extends AppCompatActivity {
         notificationAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(BaseQuickAdapter baseQuickAdapter, View view, int i) {
-                Intent intent = new Intent(NotificationActivity.this,HotSpotDetailActivity.class);
-                startActivity(intent);
+               getHotSpot(String.valueOf(notificationAdapter.getItem(i).getCom_hs()));
             }
         });
         notificationsRV.setAdapter(notificationAdapter);
@@ -153,11 +169,51 @@ public class NotificationActivity extends AppCompatActivity {
         notificationReadedAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(BaseQuickAdapter baseQuickAdapter, View view, int i) {
-                Intent intent = new Intent(NotificationActivity.this,HotSpotDetailActivity.class);
-                startActivity(intent);
+                getHotSpot(String.valueOf(notificationReadedAdapter.getItem(i).getCom_hs()));
             }
         });
         notificationsReadedRV.setAdapter(notificationReadedAdapter);
+    }
+
+    private void changeNotificationState(){
+        List<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+        for(UserNotification userNotification:notificationsNotReaded){
+            HashMap<String, Object> UserNotificationData = new HashMap<>();
+            UserNotificationData.put("notice_status",1);
+            UserNotificationData.put("notice_user",userNotification.getNotice_user());
+            UserNotificationData.put("user_nickname",userNotification.getUser_nickname());
+            UserNotificationData.put("notice_comment",userNotification.getNotice_comment());
+            UserNotificationData.put("notice_id",userNotification.getNotice_id());
+            UserNotificationData.put("com_time",userNotification.getCom_time());
+            UserNotificationData.put("com_user",userNotification.getCom_user());
+            UserNotificationData.put("com_hs",userNotification.getCom_hs());
+            UserNotificationData.put("com_content",userNotification.getCom_content());
+
+            objectService
+                    .haveReadNotification(JSONRequestBodyGenerator.getJsonObjBody(UserNotificationData))
+                    .subscribeOn(Schedulers.newThread())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Subscriber<RemoteDBOperationResponse>() {
+                        @Override
+                        public void onCompleted() {
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+                            Log.e("NotificationActivity","changeNotificationState",e);
+                        }
+
+                        @Override
+                        public void onNext(RemoteDBOperationResponse remoteDBOperationResponse) {
+                            if(!remoteDBOperationResponse.isSuccess()){
+                                Toast.makeText(NotificationActivity.this,"通知状态改变失败",Toast.LENGTH_SHORT);
+                            }
+                            else{
+                                Toast.makeText(NotificationActivity.this,"通知状态改变成功",Toast.LENGTH_SHORT);
+                            }
+                        }
+                    });
+        }
     }
 
     class MyLayoutManager extends LinearLayoutManager {
