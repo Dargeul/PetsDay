1ecabb2a57048a35500caeb90eade812599baf53
郭瑶佳 <1483197714@qq.com>
2018-01-07 21:31:49 +0800

    在查看他人页面的时候添加了关注的逻辑。

--- app/src/main/java/com/example/gypc/petsday/OthersHomePageActivity.java
+++ app/src/main/java/com/example/gypc/petsday/OthersHomePageActivity.java
@@ -17,11 +17,16 @@ import com.example.gypc.petsday.adapter.OthersPetAdapter;
 import com.example.gypc.petsday.factory.ObjectServiceFactory;
 import com.example.gypc.petsday.model.Hotspot;
 import com.example.gypc.petsday.model.Pet;
+import com.example.gypc.petsday.model.RemoteDBOperationResponse;
 import com.example.gypc.petsday.service.ObjectService;
+import com.example.gypc.petsday.utils.AppContext;
+import com.example.gypc.petsday.utils.JSONRequestBodyGenerator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import retrofit2.adapter.rxjava.Result;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
@@ -37,9 +42,14 @@ public class OthersHomePageActivity extends AppCompatActivity {
     private TextView userNickname;
 
     private Bundle hotspotDeatilInfo;
-
+    private int userID;
     private List<Pet> othersPet;
+    private List<Pet> followPet;
+    private Pet petFollow;
+    private boolean isClicked = false;
 
+    private ImageView likeIV;
+    private TextView followTV;
     private ObjectService objectService;
 
     @Override
@@ -48,18 +58,23 @@ public class OthersHomePageActivity extends AppCompatActivity {
         setContentView(R.layout.others_homepage);
 
         objectService = ObjectServiceFactory.getService();
+        followPet = AppContext.getInstance().getFollowpets();
+
         hotspotDeatilInfo = getIntent().getExtras();
 
         petsListRV = (RecyclerView)findViewById(R.id.petListRV);
         back = (ImageView)findViewById(R.id.back);
         userNickname = (TextView)findViewById(R.id.userNickname);
-
         userNickname.setText(hotspotDeatilInfo.getString("user_nickname"));
+
+        userID = (int) AppContext.getInstance().getLoginUserInfo().get("user_id");
 
         LinearLayoutManager layoutManager1 = new LinearLayoutManager(OthersHomePageActivity.this);
         othersPetAdapter = new OthersPetAdapter(R.layout.others_pet);
         petsListRV.setLayoutManager(layoutManager1);
         getOthersPet();
+        getFollowPet();
+        petsListRV.setAdapter(othersPetAdapter);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -95,6 +110,37 @@ public class OthersHomePageActivity extends AppCompatActivity {
                 Toast.makeText(OthersHomePageActivity.this, "OthersPet:" + position, Toast.LENGTH_SHORT).show();
             }
         });
+
+        othersPetAdapter.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener() {
+            @Override
+            public void onItemChildClick(BaseQuickAdapter baseQuickAdapter, View view, int i) {
+                petFollow = othersPetAdapter.getItem(i);
+                likeIV = (ImageView)view;
+                followTV = (TextView)othersPetAdapter.getViewByPosition(petsListRV,i,R.id.followTV);
+                isClicked = true;
+                getFollowPet();
+            }
+        });
+    }
+
+    private void changePetState(){
+        boolean isFollowed = false;
+        for(Pet pet1:othersPet){
+            for(Pet pet2:followPet){
+                if(pet1.getPet_id() == pet2.getPet_id()){
+                    ImageView imageView = (ImageView)othersPetAdapter.getViewByPosition(petsListRV,othersPet.indexOf(pet1),R.id.loveIV);
+                    imageView.setImageResource(R.drawable.like_fill);
+                    isFollowed = true;
+                }
+            }
+            if(isFollowed == false){
+                ImageView imageView = (ImageView)othersPetAdapter.getViewByPosition(petsListRV,othersPet.indexOf(pet1),R.id.loveIV);
+                imageView.setImageResource(R.drawable.like);
+            }
+            else{
+                isFollowed = false;
+            }
+        }
     }
 
     public void getOthersPet() {
@@ -118,9 +164,125 @@ public class OthersHomePageActivity extends AppCompatActivity {
                     @Override
                     public void onNext(List<Pet> pets) {
                         othersPet = new ArrayList(pets);
-                        othersPetAdapter.addData(pets);
-                        petsListRV.setAdapter(othersPetAdapter);
+                        othersPetAdapter.setNewData(pets);
+                        changePetState();
                     }
                 });
+    }
+
+    public void getFollowPet(){
+        objectService
+                .getPetListForUser(
+                        String.valueOf(userID),
+                        String.valueOf(ObjectServiceFactory.GET_LIKE_PET_STATUS_CODE))
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<List<Pet>>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.i("OthersHomePageActivity", "getFollowPet: complete, followpets.size() = " + String.valueOf(followPet.size()));
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("OthersHomePageActivity", "getFollowPet", throwable);
+                    }
+
+                    @Override
+                    public void onNext(List<Pet> pets) {
+                        followPet = new ArrayList(pets);
+                        if(isClicked){
+                            if(checkIsFollowed() != true){
+                                follow(petFollow.getPet_id());
+                            }else{
+                                unfollow(petFollow.getPet_id());
+                            }
+                            isClicked = false;
+                        }
+                        changePetState();
+                    }
+                });
+    }
+
+    private boolean checkIsFollowed(){
+        boolean isFollowed = false;
+        for(Pet pet:followPet){
+            if(petFollow.getPet_id() == pet.getPet_id())
+                isFollowed = true;
+        }
+        return isFollowed;
+    }
+
+    private void follow(int petId) {
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("pet_id", String.valueOf(petId));
+        data.put("user_id", userID);
+
+        objectService
+                .userFansPet(JSONRequestBodyGenerator.getJsonObjBody(data))
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Result<Integer>>() {
+                    @Override
+                    public void onCompleted() {
+                        getFollowPet();
+                        getOthersPet();
+                        Log.i("PetDetailActivity", "followPet: complete");
+                        Toast.makeText(OthersHomePageActivity.this, "关注成功", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("PetDetailActivity", "followPet", throwable);
+                    }
+
+                    @Override
+                    public void onNext(Result<Integer> integerResult) {
+                        try {
+                            likeIV.setImageResource(R.drawable.like_fill);
+                        } catch (Exception e) {
+                            Log.e("PetDetailActivity", e.getMessage(), e);
+                        }
+                    }
+                });
+    }
+
+    private void unfollow(int petId) {
+        objectService
+                .cancelUserFansPet(
+                        String.valueOf(petId), String.valueOf(userID)
+                )
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<RemoteDBOperationResponse>() {
+                    @Override
+                    public void onCompleted() {
+                        getFollowPet();
+                        getOthersPet();
+                        Log.i("PetDetailActivity", "unfollowPet: complete");
+                        Toast.makeText(OthersHomePageActivity.this, "取消关注成功", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("PetDetailActivity", "unfollowPet", throwable);
+                    }
+
+                    @Override
+                    public void onNext(RemoteDBOperationResponse r) {
+                        try {
+                            likeIV.setImageResource(R.drawable.like);
+                        } catch (Exception e) {
+                            Log.e("PetDetailActivity", e.getMessage(), e);
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        getFollowPet();
+        getOthersPet();
     }
 }
